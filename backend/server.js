dea2613a8fcf0ae6c44c4b7b8920947485af2a4e
Nathan Dean <G00423489@atu.ie>
2024-11-26 15:39:29 +0000

    Added .env file to store DB connection info. Added intital connection to DB with confirmation and error catch.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+//Require the dotenv package to access sucure env variables
+require('dotenv').config()
+const mongoose = require('mongoose')
 const port = 4000
+DATABASE_URL = process.env.DATABASE_URL
 
 app.get('/', (req, res) => {
   
@@ -8,4 +12,19 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  ConnectToDB()
 })
+
+
+async function ConnectToDB() 
+{
+  try 
+  {
+    await mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log('Connected to DB');
+  } 
+  catch (err) 
+  {
+    console.log(err);
+  } 
+}
